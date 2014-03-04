abf30144cfac1d16f87506203cd5e2385f087edb
h-sugawara <nnfxt540@gmail.com>
2014-03-04 13:22:17 +0900

    changed arrays unittest file and added string and generics list unittest files

---
+++ tests/src/common/StringTest.php
@@ -0,0 +1,102 @@
+<?php
+namespace Phpingguo\ApricotLib\Tests\Src\Common;
+
+use Phpingguo\ApricotLib\Common\String;
+use Phpingguo\ApricotLib\Enums\LibEnumName;
+
+class StringTest extends \PHPUnit_Framework_TestCase
+{
+    public function providerRemoveNamespace()
+    {
+        return [
+            [ LibEnumName::VARIABLE, 'Variable' ],
+            [ LibEnumName::CHARSET, 'Charset' ],
+            [ LibEnumName::HTTP_METHOD, 'HttpMethod' ],
+            [ 'stdClass', 'stdClass' ],
+            [ '', '' ],
+            [ true, '' ],
+            [ false, '' ],
+            [ 0, '' ],
+            [ 0.0, '' ],
+            [ 0.1, '' ],
+            [ '0', '' ],
+            [ '0.0', '' ],
+            [ '0.1', '' ],
+            [ null, '' ],
+            [ [], '' ],
+            [ new \stdClass(), '' ],
+        ];
+    }
+
+    /**
+     * @dataProvider providerRemoveNamespace
+     */
+    public function testRemoveNamespace($class_name, $expected_name)
+    {
+        $this->assertSame($expected_name, String::removeNamespace($class_name));
+    }
+    
+    public function providerIsContains()
+    {
+        return [
+            [ 'hoge', [ 'hoge', 'foo', 'bar' ], true, true ],
+            [ 'hoge', [ 'hoge', 'foo', 'bar' ], false, true ],
+            [ 'HOGE', [ 'hoge', 'foo', 'bar' ], true, true ],
+            [ 'HOGE', [ 'hoge', 'foo', 'bar' ], false, false ],
+            [ 'foo', [ 'hoge', 'foo', 'bar' ], true, true ],
+            [ 'foo', [ 'hoge', 'foo', 'bar' ], true, true ],
+            [ 'FOO', [ 'hoge', 'foo', 'bar' ], true, true ],
+            [ 'FOO', [ 'hoge', 'foo', 'bar' ], false, false ],
+            [ 'bar', [ 'hoge', 'foo', 'bar' ], true, true ],
+            [ 'bar', [ 'hoge', 'foo', 'bar' ], false, true ],
+            [ 'BAR', [ 'hoge', 'foo', 'bar' ], true, true ],
+            [ 'BAR', [ 'hoge', 'foo', 'bar' ], false, false ],
+            [ '', [ 'hoge', 'foo', 'bar' ], true, false ],
+            [ '', [ 'hoge', 'foo', 'bar' ], false, false ],
+            [ 0, [ 'hoge', 'foo', 'bar' ], true, false ],
+            [ 0, [ 'hoge', 'foo', 'bar' ], false, false ],
+            [ 0.0, [ 'hoge', 'foo', 'bar' ], true, false ],
+            [ 0.0, [ 'hoge', 'foo', 'bar' ], false, false ],
+            [ 0.1, [ 'hoge', 'foo', 'bar' ], true, false ],
+            [ 0.1, [ 'hoge', 'foo', 'bar' ], false, false ],
+            [ '0', [ 'hoge', 'foo', 'bar' ], true, false ],
+            [ '0.0', [ 'hoge', 'foo', 'bar' ], true, false ],
+            [ '0.1', [ 'hoge', 'foo', 'bar' ], true, false ],
+            [ true, [ 'hoge', 'foo', 'bar' ], true, false ],
+            [ true, [ 'hoge', 'foo', 'bar' ], false, false ],
+            [ false, [ 'hoge', 'foo', 'bar' ], true, false ],
+            [ false, [ 'hoge', 'foo', 'bar' ], false, false ],
+            [ null, [ 'hoge', 'foo', 'bar' ], true, false ],
+            [ null, [ 'hoge', 'foo', 'bar' ], false, false ],
+            [ [], [ 'hoge', 'foo', 'bar' ], true, false ],
+            [ [], [ 'hoge', 'foo', 'bar' ], false, false ],
+            [ new \stdClass(), [ 'hoge', 'foo', 'bar' ], true, false ],
+            [ new \stdClass(), [ 'hoge', 'foo', 'bar' ], false, false ],
+        ];
+    }
+
+    /**
+     * @dataProvider providerIsContains
+     */
+    public function testIsContains($haystack, $needle_list, $ignore_case, $expected)
+    {
+        $this->assertSame($expected, String::isContains($haystack, $needle_list, $ignore_case));
+    }
+    
+    public function providerIsNotRegexMatched()
+    {
+        return [
+            [ 'abc def ghi', '/[c-e]/', 0, true ],
+            [ 'abc def ghi', '/[c-e]/', 1, false ],
+            [ 'abc def ghi', '/[c-e]/', false, true ],
+        ];
+    }
+
+    /**
+     * @dataProvider providerIsNotRegexMatched
+     */
+    public function testIsNotRegexMatched($search, $regex, $not_expected, $result)
+    {
+        $this->assertSame($result, String::isNotRegexMatched($search, $regex, $not_expected));
+    }
+}
---
+++ tests/src/type/generics/GenericListTest.php
@@ -0,0 +1,50 @@
+<?php
+namespace Phpingguo\ApricotLib\Tests\Src\Type\Generics;
+
+use Phpingguo\ApricotLib\Enums\Variable;
+use Phpingguo\ApricotLib\Type\Generics\GenericList;
+
+class GenericListTest extends \PHPUnit_Framework_TestCase
+{
+    public function provider()
+    {
+        return [
+            [ Variable::INTEGER, [ 1, 10, 100, 1000 ], null, 10000, 1, null ],
+            [ Variable::INTEGER, [ 1, 10, 100, 1000 ], 'abc', 10000, 1, null ],
+            [ Variable::INTEGER, [ 1, 10, 100, 1000 ], null, '', 1, 'DomainException' ],
+        ];
+    }
+
+    /**
+     * @dataProvider provider
+     */
+    public function test($type_name, $collection, $key, $item, $unset, $exception)
+    {
+        isset($exception) && $this->setExpectedException($exception);
+        
+        $list = new GenericList($type_name, $collection);
+        
+        $this->assertInstanceOf('Phpingguo\ApricotLib\Type\Generics\GenericList', $list);
+        $this->assertSame($collection, $list->toArray());
+
+        if (isset($key)) {
+            $list[$key] = $item;
+        } else {
+            $list[] = $item;
+            $key    = $list->count() - 1;
+        }
+        
+        $this->assertArrayHasKey($key, $list);
+        $this->assertSame($item, $list[$key]);
+        
+        if (isset($unset)) {
+            unset($list[$unset]);
+            
+            $this->assertArrayNotHasKey($unset, $list);
+        }
+        
+        foreach ($list as $l_key => $l_value) {
+            $this->assertSame($collection[$l_key], $l_value);
+        }
+    }
+}
