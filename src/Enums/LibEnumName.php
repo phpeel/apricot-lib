76a7d031e6cc229d6d54cc91c29b2106bd125b6f
h-sugawara <nnfxt540@gmail.com>
2014-03-17 10:39:49 +0900

    removed HttpMethod enum class file and changed LibEnumName enum class file

--- src/Enums/LibEnumName.php
+++ src/Enums/LibEnumName.php
@@ -14,9 +14,6 @@ final class LibEnumName extends Enum
     /** 列挙型「Charset」であることを示す */
     const CHARSET     = 'Phpingguo\ApricotLib\Enums\Charset';
     
-    /** 列挙型「HttpMethod」であることを示す */
-    const HTTP_METHOD = 'Phpingguo\ApricotLib\Enums\HttpMethod';
-    
     /** 列挙型「Variable」であることを示す */
     const VARIABLE    = 'Phpingguo\ApricotLib\Enums\Variable';
 }
